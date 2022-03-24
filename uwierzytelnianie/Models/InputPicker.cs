4b1ca6b77887755336286a04bdabce6bbeb9e465
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-03-24 21:44:54 +0100

    Add project files.

---
+++ uwierzytelnianie/Models/InputPicker.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uwierzytelnianie.Models
+{
+    public class InputPicker
+    {
+
+
+        [Display(Name = "Imię")]
+        [Required, MaxLength(100)]
+        public string? name { get; set; }
+
+        [Display(Name = "Rok urodzenia")]
+        [Required, Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int? date { get; set; }
+    }
+}
---
+++ uwierzytelnianie/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using uwierzytelnianie.Models;
+
+namespace uwierzytelnianie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public InputPicker inPick { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string name { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int date { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+    }
+}
