6f92ce22bc2090f5d5009cc6fc433546cc03802b
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-03-30 13:51:19 +0200

    Database connected + Last visited site all done

---
+++ uwierzytelnianie/Models/Person.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace uwierzytelnianie.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Imię")]
+        [Required]
+        [StringLength(maximumLength: 100, ErrorMessage = "Maksymalnie 100 znaków")]
+        [Column(TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Maksymalnie 100 znaków")]
+        [Column(TypeName = "varchar(100)")]
+        public string? Surname { get; set; }
+
+        [Display(Name = "Rok urodzenia")]
+        [Required]
+        [Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int Year { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool IsLeap { get; set; }
+
+
+        public bool IsLeapYear()
+        {
+            if (((this.Year % 4 == 0 && this.Year % 100 != 0) || this.Year % 400 == 0))
+            {
+                this.IsLeap = true;
+                return true;
+            }
+            else
+            {
+                this.IsLeap = false;
+                return false;
+            }
+        }
+    }
+}
---
+++ uwierzytelnianie/Pages/People/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace uwierzytelnianie.Pages.People
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
